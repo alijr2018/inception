cd1575be082295aa5a045ff55929554ead08dba5
Abdelali Rami <abrami@e3r4p14.1337.ma>
2026-07-30 16:08:36 +0100

    check

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -12,33 +12,35 @@
 # 
 
 
-set -e
+# set -e
 
-echo "Start MariaDB init"
+# echo "Start MariaDB init"
 
 # mkdir -p /run/mysqld
 # chown mysql:mysql /run/mysqld
 
+set -e
+
+echo "Start MariaDB init"
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
-fi
 
+    mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock &
 
-mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock &
+    until mysqladmin ping --socket=/run/mysqld/mysqld.sock --silent; do
+        sleep 1
+    done
 
-until mysqladmin ping --socket=/run/mysqld/mysqld.sock --silent; do
-    sleep 1
-done
-
-
-mysql --socket=/run/mysqld/mysqld.sock -u root << EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';
+    mysql --socket=/run/mysqld/mysqld.sock -u root << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
 CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
-GRANT ALL PRIVILEGES ON `${MARIADB_DATABASE}`.* TO '${MARIADB_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin --socket=/run/mysqld/mysqld.sock shutdown
+    mysqladmin --socket=/run/mysqld/mysqld.sock shutdown
+fi
 
 exec mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock
