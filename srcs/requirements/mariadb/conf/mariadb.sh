bd4efa27483c4bea38e5932fc924415fb522b08b
alijr2018 <alirami120@hotmail.fr>
2026-08-11 17:04:49 +0100

    update witch changes

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -4,19 +4,13 @@ set -e
 
 echo "Start MariaDB"
 
-if [ ! -f "/var/lib/mysql/.initialized" ]; then
+# if [ ! -f "/var/lib/mysql/.initialized" ]; then
+if [ ! -f "/var/lib/mysql/${inception}" ]; then
     echo "Init DB"
-
-    rm -fr /var/lib/mysql/*
 
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
     mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock &
-
-    until mysqladmin ping --socket=/run/mysqld/mysqld.sock --silent; do
-        sleep 1
-    done
-    echo "DB is ready"
     
     mysql --socket=/run/mysqld/mysqld.sock -u root << EOF
 CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;
@@ -25,14 +19,7 @@ GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-    # mysqladmin --socket=/run/mysqld/mysqld.sock -u root  -p"${MARIADB_ROOT_PASSWORD}"  shutdown
     mysqladmin --socket=/run/mysqld/mysqld.sock -u root shutdown
-
-    touch /var/lib/mysql/.initialized
-    echo "MariaDB initialization complete"
-
-    else
-        echo "MariaDB already initialized"
 fi
 
 echo "Start Mariadb .."
