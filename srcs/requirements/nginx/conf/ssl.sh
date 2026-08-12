fbea3148be666a0e4b13b2abb4bdd07793715e3b
Abdelali Rami <abrami@e3r2p15.1337.ma>
2026-08-12 17:08:10 +0100

    s

--- srcs/requirements/nginx/conf/ssl.sh
+++ srcs/requirements/nginx/conf/ssl.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p /etc/nginx/ssl
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/we.key -out /etc/nginx/ssl/we.crt -subj "\CN=${DOMAIN_NAME}"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/we.key -out /etc/nginx/ssl/we.crt -subj "/CN=${DOMAIN_NAME}"
 
 cat << EOF > /etc/nginx/conf.d/default.conf
   server
